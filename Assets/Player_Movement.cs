80f8e18bda7725fb038f7c6678b26db89fc8acd6
zangston <winston.y.zhang@gmail.com>
2024-03-21 14:14:55 -0400

    Fix player movement
    Player movement is now more responsive to input

--- Assets/Player_Movement.cs
+++ Assets/Player_Movement.cs
@@ -4,24 +4,23 @@ using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
 {
-    [SerializeField] private float speed = 100.0f;
-    [SerializeField] private Rigidbody2D rb;
+    private Rigidbody2D rb;
+
+    [SerializeField] private float moveSpeed = 7.0f;
+    private float dirX = 0f;
+    private float dirY = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
-
-        Vector3 tempVect = new Vector3(h, v, 0);
-        tempVect = tempVect.normalized * speed * Time.deltaTime;
-
-        rb.MovePosition(rb.transform.position + tempVect);
+        dirX = Input.GetAxisRaw("Horizontal");
+        dirY = Input.GetAxisRaw("Vertical");
+        rb.velocity = new Vector2(dirX * moveSpeed, dirY * moveSpeed);
     }
 }
